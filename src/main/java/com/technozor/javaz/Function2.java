1f317eedd45bd18160ecf2143abb7877d0c8bea3
ouertani <ouertani@gmail.com>
2014-04-15 13:00:40 +0100

    Add curring func for  f2

--- src/main/java/com/technozor/javaz/Function2.java
+++ src/main/java/com/technozor/javaz/Function2.java
@@ -1,6 +1,7 @@
 package com.technozor.javaz;
 
 import java.util.Objects;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 /**
@@ -11,9 +12,16 @@ public interface Function2<T, U, R> extends Function<T, Function<U, R>> {
 
     Function<U, R> apply(T t);
 
-    default <V> Function2<T, U, V> map(Function<? super R, ? extends V> after) {
+    default <V> Function2<T, U, V> andApply(Function<? super R, ? extends V> after) {
         Objects.requireNonNull(after);
-        return t -> (u -> after.apply(apply(t).apply(u)));
+        return t -> u -> after.apply(apply(t).apply(u));
     }
 
+    default BiFunction<T,U,R> uncurrying () {
+        return (t, u) -> apply(t).apply(u);
+    }
+
+    static <T,U,R>  Function2<T,U,R> currying (BiFunction<T,U,R> f) {
+        return t -> u -> f.apply(t,u);
+    }
 }
