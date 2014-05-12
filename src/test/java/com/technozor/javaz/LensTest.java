a7a992a535c0901ee98ae5b778274adb8dfe529c
ouertani <ouertani@gmail.com>
2014-05-12 11:45:01 +0100

    New changelist

--- src/test/java/com/technozor/javaz/LensTest.java
+++ src/test/java/com/technozor/javaz/LensTest.java
@@ -74,6 +74,15 @@ public class LensTest {
 
 
     @Test
+    public void classic() {
+        Person person = new Person("Jack", "Smith", new Address("Default", "Default", "Default", 0));
+
+        Address adress =  new Address(person.getAddress().getStreet(), person.getAddress().getCity(),person.getAddress().getState(), person.getAddress().getZipCode() +1);
+        Person p1 =  new Person(person.getFirstName(), person.getLastName(), adress);
+        assertEquals(p1.address.zipCode - 1, 0);
+    }
+
+    @Test
     public void testLenses() {
         Person person = new Person("Jack", "Smith", new Address("Default", "Default", "Default", 0));
 
